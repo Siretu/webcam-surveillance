f2fe79ce581622a4d10fc39a50b08aed4055d8c2
Erik Ihrén <erikihr@gmail.com>
2015-08-03 23:47:30 +0200

    Modified home detection script to use .home-file and script not automatically turns off when you come home, instead of having to be triggered by movement first.

--- surveillance.py
+++ surveillance.py
@@ -5,18 +5,18 @@ import email_settings
 from email.mime.text import MIMEText
 import smtplib
 import datetime
+import time
 
+TIME_THRESHOLD = 10
 
 def at_home():
-    hostnames = ["192.168.1.13","192.168.1.15"]
-
-    found = False
-    for host in hostnames:
-        cmd = "ping -c 1 " + host
-        response = os.popen(cmd).read()
-        if "64 bytes from" in response:
-            found = True
-    return found
+    with open("/home/siretu/.home") as f:
+        content = f.read()
+        if content:
+            last_time = float(content)
+            return time.time() - last_time < TIME_THRESHOLD
+    return False
+            
 
     
 def mail(email, subject, message):
@@ -40,38 +40,35 @@ def main():
     gone_since = 0
     last_alert = 0
     while 1:
-        while gone_since < 4:
-            if at_home():
-                print datetime.datetime.now().isoformat(" ") + " " + "Still home"
-                gone_since = 0
-            else:
-                gone_since += 1
+        sleep(1)
+        if at_home():
+            gone_since = 0
+            print datetime.datetime.now().isoformat(" ") + " " + "Still home"
+        else:
+            if gone_since < 4:
                 print "Couldn't reach phone, trying again. Try: %d" % gone_since
-        
-        gone_since = 0
-        i = 1
-        os.system("python takepicture.py pics/pic%d.jpg" % i)
-        i = 2
-        while 1:
-            os.system("python takepicture.py pics/pic%d.jpg" % i)
-            sleep(1)
-            _,a,b,_ = imagecompare.compare("pics/pic1.jpg","pics/pic2.jpg")
-            print datetime.datetime.now().isoformat(" ") + " " + "%d %d" % (a,b)
-            if a >= 4 and b >= 10:
-                print "Movement!"
-                if not at_home():
-                    gone_since = 4
-                    print "ALERT"
-                    os.rename("pics/pic1.jpg", "pics/save1%s.jpg" % str(int(time())))
-                    os.rename("pics/pic2.jpg", "pics/save2%s.jpg" % str(int(time())))
-                    if time() - last_alert > 600:
-                        last_alert = time()
-                        print "Sent email"
-                        mail(email_settings.username, "Movement detected", "Detected movement at home")
-                    os.system('espeak "Movement detected"')
-                break
-            
-            i = 3 - i
+                gone_since += 1
+            else:
+                i = 1
+                os.system("python takepicture.py pics/pic%d.jpg" % i)
+                i = 2
+                sleep(1)
+                os.system("python takepicture.py pics/pic%d.jpg" % i)
+                _,a,b,_ = imagecompare.compare("pics/pic1.jpg","pics/pic2.jpg")
+                print datetime.datetime.now().isoformat(" ") + " " + "%d %d" % (a,b)
+                if a >= 4 and b >= 10:
+                    print "Movement!"
+                    if not at_home():
+                        gone_since = 4
+                        print "ALERT"
+                        os.rename("pics/pic1.jpg", "pics/save1%s.jpg" % str(int(time())))
+                        os.rename("pics/pic2.jpg", "pics/save2%s.jpg" % str(int(time())))
+                        if time() - last_alert > 600:
+                            last_alert = time()
+                            print "Sent email"
+                            mail(email_settings.username, "Movement detected", "Detected movement at home")
+                        os.system('espeak "Movement detected"')
+                i = 3 - i
 
 
 
